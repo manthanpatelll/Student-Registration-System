cade9fe7beb1937289face7f5d09ae34b66f6208
Oluwajolasun Jaiyesimi <51948882+oluwajolasun@users.noreply.github.com>
2023-07-13 02:32:37 -0600

    completed course controller

--- server/controllers/courses.js
+++ server/controllers/courses.js
@@ -6,7 +6,7 @@ const Course = db.models.Courses;
 //Get all courses {GET}
 const getAllCourses = async (req, res) => {
   try {
-    const courses = await Course.findAll();
+    const courses = await Course.findAll();     //From docs model.findall() returns an array of the models 
     res.status(200).json({ courses });
   } catch (error) {
     res.status(500).json({ msg: error });
@@ -17,8 +17,8 @@ const getAllCourses = async (req, res) => {
 const getSingleCourse = async (req, res) => {
   try {
     let id = req.params.id;
-    const course = await Course.findOne({ where: { id } });
-    console.log(course);
+    const course = await Course.findOne({ where: { id } });     //from docs model.findone() return the model if condition was met other null
+    // console.log({ course });
     if (course === null) {
       return res.status(404).json({ msg: `No course with id ${id} ` });
     }
@@ -31,7 +31,7 @@ const getSingleCourse = async (req, res) => {
 //Create course {POST}
 const addCourse = async (req, res) => {
   try {
-    const course = await Course.create(req.body);
+    const course = await Course.create(req.body);       //from docs model.create returns the model 
     return res.status(200).json(course);
   } catch (error) {
     return res.status(500).json({ msg: error });
@@ -39,13 +39,18 @@ const addCourse = async (req, res) => {
 };
 
 const updateCourse = async (req, res) => {
-  let id = req.params.id;
-
-  if (req.params.id) {
-    const course = await Course.update(req.body, { where: { id } });
-    return res.status(200).json(course);
-  } else {
-    return res.status(400).json({ msg: "invalid course id" });
+  try {
+    let id = req.params.id;
+    const course = await Course.update(req.body, { where: { id } }); //From docs model.update returns an array where the first element is number oif affected rows
+    // console.log(course);
+    if (course[0] === 0) {
+      return res
+        .status(200)
+        .json({ msg: `Course id ${id} has already been updated` });
+    }
+    res.status(200).json({ id, data: req.body });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
   }
 };
 
@@ -53,12 +58,12 @@ const updateCourse = async (req, res) => {
 const deleteCourse = async (req, res) => {
   try {
     let id = req.params.id;
-    const course = await Course.destroy({ where: { id } });
-    console.log(course);
+    const course = await Course.destroy({ where: { id } }); //From docs model.destroy returns a number which is the number of rows deleted
+    // console.log(course);
     if (course === 0) {
       return res.status(404).json({ msg: `No course with id ${id} ` });
     }
-    res.status(200).json({ course });
+    res.status(200).json({ msg: `Deleted course with id ${id}` });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
