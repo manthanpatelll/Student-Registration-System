a45ad457b04b833c2b84ecbc598e49f4a9832448
Oluwajolasun Jaiyesimi <51948882+oluwajolasun@users.noreply.github.com>
2023-07-13 17:39:08 -0600

    Added not found 404 middleware route

--- server/app.js
+++ server/app.js
@@ -3,22 +3,19 @@ const app = express();
 const courses = require("./routes/courses");
 const student = require("./routes/students");
 const registration = require("./routes/registration");
-const db = require("./models");
-
-app.get("/", (req, res) => {
-  console.log("made a request");
-  res.status(200).send("Course Registration");
-});
+const notFound = require("./middleware/not_found");
+require("dotenv").config();
 
 //middleware
+app.use(notFound);
 app.use(express.json());
 
 //routes
 app.use("/api/v1/courses", courses, registration);
 app.use("/api/v1/students", student);
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
 });
